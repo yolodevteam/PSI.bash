f4d2270322646c6aa42d4e98e0a954151e015629
Zhongfu Li <me@zhongfu.li>
2013-11-17 09:52:04 +0800

    oops, forgot the other psi stuff

---
+++ bin/in/morepsi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+## other PSI stuff: other pollutants, PSI per area etc. - in/morepsi.sh ##
+phantomjs morepsi/fetch.js
+echo morepsi module finished.
--- etc/config/config.sh
+++ etc/config/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## settings for PSI script ##
 # input modules to run
-config_main_inmod="psi"
+config_main_inmod="psi morepsi"
 # output modules to run
 config_main_outmod="html pushover json"
 
