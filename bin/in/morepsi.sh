d4431b875523ae7cefa8436e6b82c2ebfcf48f7e
Zhongfu Li <me@znx.cc>
2013-11-17 11:54:26 +0800

    fixed shit

--- bin/in/morepsi.sh
+++ bin/in/morepsi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## other PSI stuff: other pollutants, PSI per area etc. - in/morepsi.sh ##
-$morepsi="bash morepsi/run.sh"
+$morepsi="$(bash morepsi/run.sh)"
 cat << EOF > /var/www/psi/all.json
 {
 $(cat /var/www/psi/all.json | tail -n 24 | sed '$ d'),
