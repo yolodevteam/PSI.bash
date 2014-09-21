01027f13064857193e88cb847e07a3325db0988a
Zhongfu Li <me@znx.cc>
2014-09-22 00:35:59 +0800

    moved tools to bin/tools, added new tool to generate all.json (morepsi)

--- bin/tools/newtoken.sh
+++ bin/tools/newtoken.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #### token creation script ####
 #### creates google spreadsheet token, used for setting spreadsheet info ####
-. ../etc/config/gdrive.sh
+. ../../etc/config/gdrive.sh
 #### start script
 curl https://www.google.com/accounts/ClientLogin -d Email="$config_gdrive_email" -d Passwd="$config_gdrive_password" -d accountType=GOOGLE -d source=cURL -d service=wise | grep Auth | cut -d\= -f2 > ../etc/gdrivetoken
 echo "Token generated."
