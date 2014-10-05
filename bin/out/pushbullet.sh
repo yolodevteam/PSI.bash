b821c283a3850afb8c34bb3679982a372be35a0e
Zhongfu Li <me@znx.cc>
2014-10-06 02:13:34 +0800

    out/pushbullet: changed time format

--- bin/out/pushbullet.sh
+++ bin/out/pushbullet.sh
@@ -3,5 +3,5 @@
 # load config
 . etc/config/pushbullet.sh
 # pushbullet
-curl -s -u ${config_pushbullet_accesstoken}: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary "{\"type\": \"note\", \"channel_tag\": \"${config_pushbullet_channel}\", \"title\": \"PSI at $(date +"%d-%m-%Y %H:%M")\", \"body\": \"PSI for $(date +%H:%M) is ${psi}\"}"
+curl -s -u ${config_pushbullet_accesstoken}: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary "{\"type\": \"note\", \"channel_tag\": \"${config_pushbullet_channel}\", \"title\": \"PSI at $(date +"%d-%m-%Y %H:00")\", \"body\": \"PSI for $(date +%H:00) is ${psi}\"}"
 echo Pushbullet module finished.
